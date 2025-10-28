84c4e4a44fb60861f82a6c93ef051627cd305535
Thomas Garrafa <garrafa435@gmail.com>
2025-10-28 15:42:45 +0100

    Add SoAoS_Classic.cpp to compare SoA and AoS performance
    Added a benchmark that measures the execution time of Structure of Arrays (SoA)
    versus Array of Structures (AoS) using the computeTime utility.
    
    Includes:
    - Implementation of SoA and AoS data layouts
    - Timing of initialization and summation loops
    - Output of average execution times for both approaches

---
+++ dynamic/SoAoS_Classic.cpp
@@ -0,0 +1,97 @@
+#include "ComputeTime.h"
+#include <string.h>
+#include <stdio.h>
+#include <utility>
+
+using namespace std;
+
+#define SIZE 10000000
+#define TIMES 100
+
+struct SoA{
+    int *a;
+    float *b; 
+    char *c;
+    double *padding1;
+    float *padding2;
+    double *padding3;
+    int size;
+
+    SoA(int size){
+        this->size = size;
+        a = new int[size];
+        b = new float[size]; 
+        c = new char[size];
+        padding1 = new double[size];
+        padding2 = new float[size];
+        padding3 = new double[size];
+    }
+
+    ~SoA(){
+        delete[]a;
+        delete[]b;
+        delete[]c;
+        delete[]padding1;
+        delete[]padding2;
+        delete[]padding3;
+    }
+};
+
+struct Data{
+    int a;
+    float b;
+    char c;
+    double padding1;
+    float padding2;
+    double padding3;
+
+    Data(){
+        a = 0;
+        b = 0.0;
+        c = ' ';
+    }
+};
+
+struct AoS{
+    Data* data;
+    int size;
+
+    AoS(int size){
+        this->size = size;
+        data = new Data[size];
+    }
+
+    ~AoS(){
+        delete[] data;
+    }
+};
+
+int main() {
+    SoA soa(SIZE);
+    AoS aos(SIZE);
+
+    double sumA = 0;
+    double sumB = 0;
+
+    auto aos_func = [&]() {
+        for (int i = 0; i < SIZE; ++i) { aos.data[i].a = i; }
+        for (int i = 0; i < SIZE; ++i) { aos.data[i].b = 0.5f * i; }
+
+        for (int i = 0; i < SIZE; ++i) { sumA += aos.data[i].a; }
+        for (int i = 0; i < SIZE; ++i) { sumB += aos.data[i].b; }
+    };
+
+    auto soa_func = [&]() {
+        for (int i = 0; i < SIZE; ++i) { soa.a[i] = i; }
+        for (int i = 0; i < SIZE; ++i) { soa.b[i] = 0.5f * i; }
+
+        for (int i = 0; i < SIZE; ++i) { sumA += soa.a[i]; }
+        for (int i = 0; i < SIZE; ++i) { sumB += soa.b[i]; }
+    };
+    
+    double time_aos = computeTime(aos_func, TIMES);
+    double time_soa = computeTime(soa_func, TIMES);
+
+    printf("AOS average time elapsed: %f\n", time_aos);
+    printf("SOA average time elapsed: %f\n", time_soa);
+}
